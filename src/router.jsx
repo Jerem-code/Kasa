2dcb5473506db2e53a9e01a72de6ee50aac44937
Jerem-code <jeremy.pitton@wanadoo.fr>
2024-12-31 16:19:28 +0100

    refactor: reorganize component folder structure

--- src/router.jsx
+++ src/router.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/header.jsx'
-import Footer from './components/Header/footer.jsx'
+import Footer from './components/Footer/footer.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
