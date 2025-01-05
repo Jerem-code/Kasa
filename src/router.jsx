d9672983c41d44872144fb51ca2eab17e98843d4
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-05 19:40:44 +0100

    feat:Setting banner and base layout

--- src/router.jsx
+++ src/router.jsx
@@ -4,14 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/header.jsx'
 import Footer from './components/Footer/footer.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer />
+      <div className="main-container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   </StrictMode>,
 )
