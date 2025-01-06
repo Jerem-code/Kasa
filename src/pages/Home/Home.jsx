24e83285f85dee01c855bdbd7418cccbe09de6d7
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-06 17:33:41 +0100

    feat: add accommodation cards with grid layout and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Banner from "../../components/Banner/banner";
+import Card from "../../components/Card/card";
+import logements from "../../datas/logements.json";
 import "../Home/home.scss";
 import backgroundHeader from "../../assets/backgroundHeader.png";
 
@@ -7,8 +9,18 @@ function Home() {
   return (
     <div className="home-layout">
       <Banner title="Chez vous, partout et ailleurs" backgroundHeader={backgroundHeader} alt="bannière" />
+      <div className="cards-grid">
+        {logements.map(logement => (
+          <Card
+            key={logement.id}
+            id={logement.id}
+            title={logement.title}
+            cover={logement.cover}
+          />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
