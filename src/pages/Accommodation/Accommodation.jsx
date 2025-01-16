d72598b1249c551ecc1e26713d998a6e94badd2f
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-16 18:52:35 +0100

    removed imports from components

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Carousel from '../../components/Carousel/carousel';
 import accommodations from '../../datas/logements.json';
 import './accomodation.scss';
