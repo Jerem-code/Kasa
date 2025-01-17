b0de4860a0a81d9c74ca1eaa4659e1c6f16be4ee
Jerem-code <jeremy.pitton@wanadoo.fr>
2025-01-17 15:27:05 +0100

    feat Deploy setup

--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { HashRouter as Router } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/header.jsx'
 import Footer from './components/Footer/footer.jsx'
 import About from './pages/About/about.jsx'
-//import Error404 from './pages/Error404/error404.jsx'
 import ErrorComponent from "./components/Error/error.jsx"
 import Accommodation from './pages/Accommodation/Accommodation.jsx'
 import '@fortawesome/fontawesome-svg-core/styles.css';
